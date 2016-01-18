73bdff432b481bbd83cc4d60e276c4b845f28121
Manish Bellani <manish.bellani@gmail.com>
2016-01-18 17:35:05 -0600

    add foldLeft, foldRight and some experiements that go along with it

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -62,6 +62,7 @@ object List {
     go(lst, 0)
   }
 
+  @tailrec
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] =
     l match {
       case Cons(h, t) if f(h) => dropWhile(t, f)
@@ -77,4 +78,78 @@ object List {
     }
   }
 
+  //this functions declares two generic types, A,B.question that i had for myself was why is type of input list
+  // element different than the output type. applying a bit more pressure on System-2 revealed that:
+  // It is always possible to have output type to be different because the list will not always be like List of Ints
+  // that need to summed or List of doubles that need to be multiplied. It could also be a list of Person class
+  // instances where an average age in the list needs to be computed.Keeping input list type and the output separate
+  // allows that.
+  def foldRight[A, B](as: List[A], z: B)
+                     (f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ns: List[Int]) = foldRight(ns, 0)((x, y) => x + y)
+
+  def product2(ns: List[Double]) = foldRight(ns, 1.0)((x, y) => x * y)
+
+  //Exercise 3.7
+  //Can we introduce short circuiting in foldRight so that it returns 0 immediately when it encounters it.
+
+  //Approach 1:
+  //We could add a case in foldRight like Cons(0,xs) but the short coming of this approach is that we're now
+  //hard coding that logic in foldRight that makes it more type dependent e.g. Double or Int. This would not
+  //be a preferred approach
+
+  //Approach 2:
+  //Allow passing of another "short circuit value" into foldRight that allows such short circuit checking. If
+  //we do that then adding a case statement like Case(ssValue,_) will allow us to directly return a 0 and halt
+  //execution. This although has limitations, for example: in case of Ints we may not want to short circuit an
+  //operation even if there is a 0 in the list. We could possibly add extra checks and guards in foldLeft but
+  //that might kill elegance of the method as it stands currently.
+
+  def length[A](as: List[A]): Int = {
+    foldRight(as, 0)((_, acc) => acc + 1)
+  }
+
+
+  //May be useless but an alternative implementation of foldLeft that i came up with
+  //what probably fooled me here is that the name of the default argument. i got into thinking that
+  //default can only be used as default value but not accumulator.
+  def foldLeft_Confused[A, B](as: List[A], default: B)
+                             (f: (B, A) => B): B = {
+    @tailrec
+    def go(as: List[A], default: B, acc: B): B = as match {
+      case Nil => acc
+      case Cons(x, xs) => go(xs, default, f(acc, x))
+    }
+    go(as, default, default)
+  }
+
+
+  //solution from fp scala git repo.
+  @tailrec
+  def foldLeft[A, B](as: List[A], z: B)
+                    (f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+
+  }
+
+  def sum3(ns: List[Int]) = foldLeft(ns, 0)((x, y) => x + y)
+
+  def product3(ns: List[Int]) = foldLeft(ns, 1)((x, y) => x * y)
+
+  def length3[A](as: List[A]): Int = {
+    foldLeft(as, 0)((x, _) => x + 1)
+  }
+
+  def reverse[A](as: List[A]): List[A] = {
+    foldLeft(as, List[A]())((x, y) => Cons(y, x))
+  }
+
+  def append[A](l: List[A], r: List[A]): List[A] = {
+    foldRight(l, r)((head, tail) => Cons(head, tail))
+  }
 }
--- src/test/scala/ListSpec.scala
+++ src/test/scala/ListSpec.scala
@@ -56,4 +56,44 @@ class ListSpec extends FlatSpec {
     assert(List.count(lst) == 3)
   }
 
+  it should "be a surprise when we pass Nil and Cons themselves to foldRight" in {
+    val value = List.foldRight(List(1, 2, 3, 4), Nil: List[Int])((Cons(_, _)))
+    println(value)
+  }
+
+  it should "return length of 3" in {
+    val lst = List(1, 2, 3)
+    assert(List.length(lst) == 3)
+  }
+
+  it should "sum numbers correctly with foldLeft" in {
+    val lst = List(1, 2, 3)
+    assert(List.sum3(lst) == 6)
+  }
+
+  it should "multiply numbers correctly with foldRight" in {
+    val lst = List(1, 2, 3, 4)
+    assert(List.product3(lst) == 24)
+  }
+
+  it should "return length when using foldLeft in length3" in {
+    val lst = List(1, 2, 3, 4)
+    assert(List.length3(lst) == 4)
+  }
+
+  it should "return a reverse list" in {
+    val lst = List(1, 2, 3, 4)
+    println("reversed" + List.reverse(lst))
+
+  }
+
+  it should "should append to list" in {
+    val lst1 = List(1, 2, 3, 4)
+    val lst2 = List(5, 6, 7)
+    val lst = List.append(lst1, lst2)
+    println("appended " + lst)
+    assert(List.length3(lst) == 7)
+
+  }
+
 }
